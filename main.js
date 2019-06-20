618f1bf6f22acc7124501c0e324799443190a870
Bartłomiej Pogodziński <bartek.pogod@gmail.com>
2019-06-20 15:42:56 +0200

    Add progress bar

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 const game = require("./game.js");
 const inputInitPromise = require("./input-init.js");
 
-const gamesNumber = 10000;
+const _cliProgress = require("cli-progress");
+
+const gamesNumber = 100000;
 
 inputInitPromise.then(algorithmNames => {
   let wins = algorithmNames.map(() => 0);
+  const progressBar = new _cliProgress.Bar({}, _cliProgress.Presets.shades_classic);
+  progressBar.start(gamesNumber, 0);
+
   // console.log(wins);
 
   for (let i = 1; i <= gamesNumber; i++) {
     winner = game([...algorithmNames]);
+    progressBar.increment(1);
+
     if (winner >= 0) {
       wins[winner]++;
-      console.log(i+" of "+gamesNumber+": "+algorithmNames[winner]);
-    } else
-      console.log("game " + i + " terminated");
+      //console.log(i+" of "+gamesNumber+": "+algorithmNames[winner]);
+    } //else
+      //console.log("game " + i + " terminated");
 
   }
+
+  progressBar.stop();
 
   console.log();
   for (let i = 0; i < algorithmNames.length; i++)
